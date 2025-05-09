283a83a68fe175af557878c9ed8cf4f1d272a647
Tybault <tybault.hollanders@gmail.com>
2025-05-09 22:21:01 +0200

    small change to params

--- app/api/insurance-simulations/[simId]/route.ts
+++ app/api/insurance-simulations/[simId]/route.ts
@@ -21,7 +21,7 @@ export async function GET(request: Request, { params }: Params) {
       return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
     }
 
-    const { simId } = await params;
+    const { simId } = params;
     const url = new URL(request.url);
     const loanId = url.searchParams.get('loanId');
 
